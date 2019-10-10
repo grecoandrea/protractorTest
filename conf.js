8e0d33e6a9b4d854095966441a533892c4640b4b
Andreag <43850494+grecoandrea@users.noreply.github.com>
2019-10-10 16:54:47 +0200

    correction on test name

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   directConnect: true,
-  specs: ['SimpleSearch.js'],
+  specs: ['simpleSearch.js'],
 
   capabilities: {
     'browserName': 'chrome'
